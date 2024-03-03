76d610fae3fc7f91431f610ed3e810fef90d23b8
Stanislav Malakhov <phoenix0597@ya.ru>
2024-03-03 22:02:30 +0300

    Rest practice tasks оf 12 module

--- 12th-module/12.4-man.py
+++ 12th-module/12.4-man.py
@@ -1,10 +1,22 @@
 class Person:
+    """ Базовый класс, описывающий человека.
+    __count - счетчик общего количества человек
+    Args:
+        name (str): Имя человека
+        age (int): Возраст человека
+    
+    Attributes:
+        max_count (int): Максимальное количество людей.
+    """
     __count = 0
+    max_count = 5
     
     def __init__(self, name, age):
         self.__age = None
         self.__name = name
         self.set_age(age)
+        if Person.__count >= self.max_count:
+            raise ValueError("Слишком много людей.")
         Person.__count += 1
     
     def __str__(self):
@@ -15,12 +27,25 @@ class Person:
         return Person.__count
     
     def set_age(self, age):
+        """
+        Сеттер для установления возраста
+        :param age: возраст
+        :type age: int
+        :return: None
+        :rtype: None
+        :raises: ValueError: Если возраст не входит в диапазон от 0 до 100, то генерируется исключение
+        """
         if 0 <= age <= 100:
             self.__age = age
         else:
             raise ValueError("Недопустимый возраст (должен быть в диапазоне от 0 до 100).")
     
     def get_age(self):
+        """
+        Геттер для получения возраста
+        :return: __age
+        :rtype: int
+        """
         return self.__age
     
     def get_name(self):
@@ -41,6 +66,20 @@ class Student(Person):
 
 
 class Employee(Person):
+    """
+    Класс, описывающий работника. Родитель - Person
+    Args:
+        name (str): Имя человека
+        age (int): Возраст человека
+        company (str): Название компании
+        salary (int): Зарплата
+    
+    Attributes:
+        __company (str): Название компании, в которой работает работник
+        __salary (int): Зарплата работника
+    
+    
+    """
     def __init__(self, name, age, company, salary):
         super().__init__(name, age)
         self.__company = company
@@ -54,11 +93,13 @@ class Employee(Person):
         return info
 
 
-student = Student(name='Миша', age=25, university='ФизТех')  # явное лучше, чем неявное!
-print(student)
-
-# хорошим тоном является - при создании объекта с помощью именованных параметров указывать, что конкретно мы передаём
-employee = Employee(name='Вася', age=30, company='Google', salary=10000)
-print(employee)
+# student = Student(name='Миша', age=25, university='ФизТех')  # явное лучше, чем неявное!
+# print(student)
+#
+# # хорошим тоном является - при создании объекта с помощью именованных параметров указывать, что конкретно мы передаём
+# employee = Employee(name='Вася', age=30, company='Google', salary=10000)
+# print(employee)
 
 # Использование более 7 параметров является плохим тоном! Это показывает, что метод или функция плохо спроектированы
+
+print(Person.__doc__)
